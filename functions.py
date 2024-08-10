b8a01d31f3a4fd6e33942b4ba75cc26799ed2df8
John Raugas <didascolia@gmail.com>
2024-08-10 10:35:46 +0400

    Add functions module (add files)

---
+++ functions.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+
+def factorial(x):
+    """Return the factorial of x."""
+    ans = 1
+    i = 2
+    while i <= x:
+        ans *= i
+        i += 1
+    return Decimal(ans)
+
+
+def permutations(n, k):
+    """Return the number of k-permutations on a set of n elements."""
+    ans = 1
+    i = 0
+    while i < k:
+        ans *= (n - i)
+        i += 1
+    return Decimal(ans)
+
+
+def combinations(n, k):
+    """Return the number of k-combinations on a set of n elements."""
+    return permutations(n, k) / factorial(n, k)
