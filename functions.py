8a01cf2ba8e3baf2626e678b44ec0d9261f37409
John Raugas <didascolia@gmail.com>
2024-09-09 23:52:52 +0400

    Organize functions.py

--- functions.py
+++ functions.py
@@ -11,17 +11,20 @@ import mathfunctions as mf
 
 
 functions = [
-    # these are the default tokens
-    Token(',', Vector.join,         2, 0, 0, 'oper'),
+    # Essential tokens
     Token('+', lambda a, b: a + b,  2, 1, 0, 'oper'),
     Token('-', lambda a, b: a - b,  2, 1, 0, 'oper'),
     Token('*', lambda a, b: a * b,  2, 2, 0, 'oper'),
     Token(':', lambda a, b: a / b,  2, 2, 0, 'oper'),
-    Token('/', lambda a, b: a / b,  2, 0, 0, 'oper'),
     Token('^', lambda a, b: a ** b, 2, 3, 1, 'oper'),
-    Token('mod', lambda a, b: a % b,   2, 2, 0, 'oper'),
     Token('_neg_', lambda a: -a,       1, 3, 1, 'oper'),
     Token('_dot_', lambda a, b: a * b, 2, 3, 1, 'oper'),
+
+    # Default tokens
+    Token('!', mf.factorial,         1, 4, 0, 'sign'),
+    Token(',', Vector.join,          2, 0, 0, 'oper'),
+    Token('/', lambda a, b: a / b,   2, 0, 0, 'oper'),
+    Token('mod', lambda a, b: a % b, 2, 2, 0, 'oper'),
     Token('sin',    Quantity.sin,    1, 3, 1, 'func'),
     Token('cos',    Quantity.cos,    1, 3, 1, 'func'),
     Token('tan',    Quantity.tan,    1, 3, 1, 'func'),
@@ -29,9 +32,8 @@ functions = [
     Token('arccos', Quantity.arccos, 1, 3, 1, 'func'),
     Token('arctan', Quantity.arctan, 1, 3, 1, 'func'),
     Token('sin ^', lambda a, b: Quantity.sin(a) ** b, 2, 3, 1, 'func'),
-    Token('cos ^', lambda a, b: Quantity.sin(a) ** b, 2, 3, 1, 'func'),
-    Token('tan ^', lambda a, b: Quantity.sin(a) ** b, 2, 3, 1, 'func'),
+    Token('cos ^', lambda a, b: Quantity.cos(a) ** b, 2, 3, 1, 'func'),
+    Token('tan ^', lambda a, b: Quantity.tan(a) ** b, 2, 3, 1, 'func'),
 
-    # place your custom tokens here
-    Token('!', mf.factorial, 1, 4, 0, 'sign'),
+    # Place your custom tokens here
 ]
