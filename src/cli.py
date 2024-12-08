f2b43d7aaf2cee6d3131035250c24145bc12c05c
John Raugas <didascolia@gmail.com>
2024-12-08 13:59:34 +0400

    feat: Write a prettier CLI

---
+++ src/cli.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""This script contains a command line interface for clic calculator.
+
+It runs the calculator with a prompt.
+"""
+
+from calculator import Calculator
+import symbols as smbs
+
+
+def prompt():
+    """A minimal prompt for the calculator."""
+    helptext = 'Basic help:\n'
+    helptext += smbs.cc['command'] + 'q -- quit,\n'
+    helptext += smbs.cc['command'] + 'l f -- list available functions,\n'
+    helptext += smbs.cc['command'] + 'l u -- list available units,\n'
+    helptext += smbs.cc['command'] + 'h <FUNCTION> -- help on a function.\n'
+    ctor = Calculator({'help': helptext})
+    print(',~~~~~~~~~~~~~~~~~~~~~~~~~~~~~,')
+    print('| Welcome to clic calculator! |')
+    print("| Type 'help' for basic help, |")
+    print('|    please see MANUAL.md     |')
+    print("'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'")
+    while True:
+        exp = input('\033[1;32mclic:\033[0m ')
+        ctor.calculate(exp)
+        flag, ans = ctor.get_answer()
+        if flag:
+            print(f'! {ans}')
+        elif ctor.silent:
+            continue
+        else:
+            print(f'= {ans}')
+        print()
+
+
+if __name__ == '__main__':
+    try:
+        # readline is used to improve standard input UX
+        import readline
+    except ImportError:
+        pass
+    prompt()
